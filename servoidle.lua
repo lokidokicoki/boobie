f74eb36687e15b1cdc8bb61e52ca1e0378911a31
Robin Tregaskis <lokidokicoki@gmail.com>
2011-10-31 20:00:57 +0000

    Cleaned up help, removed dead code better variable names

--- servoidle.lua
+++ servoidle.lua
@@ -1,39 +1,36 @@
 --- Use iostat (from sysstat package) to get/display CPU idle percentage.
+-- Requires Archonixs Boobie Servo v1.0
 module('servoidle', package.seeall)
 require 'log'
 require 'serial'
--- boobie has 9 leds
+
 local rate=1
 local interval=0.1
 local samples=1
-local curr_leds=0
-local prev_leds=0
-local SERVO = 15
---max steps = 24
+local curr_idle=0
+local prev_idle=0
+local SERVO_PIN = 15
 
 local MIN_STEP = 0 
 local MAX_STEP = 23
 
---- (Re)set LEDs count trackers
+--- (Re)set servo and count trackers
 function configure()
---    for i=1, #active_pins, 1 do
---	serial.write('w', active_pins[i], OFF)
---    end
     serial.write('x')
-    sleep(2)
+    sleep(interval)
     for i=23, 0, -1 do
-	serial.write('w', SERVO, i)
+	serial.write('w', SERVO_PIN, i)
 	sleep(0.2)
     end
     for i=0, 23 do
-	serial.write('w', SERVO, i)
+	serial.write('w', SERVO_PIN, i)
 	sleep(0.2)
     end
-    curr_leds=0
-    prev_leds=0
+    curr_idle=0
+    prev_idle=0
 end
 
---- Call iostat, read CPU idle %, calc number of LED to display, write value to Boobie board.
+--- Call iostat, read CPU idle %, calc position of servo, write value to Boobie board.
 function process()
     os.execute("iostat -c "..rate.." ".. samples + 1 .." > /tmp/servoidle");
     local count=0
@@ -52,34 +49,20 @@ function process()
     end
 
     idle = 100-(idle/samples)
-    curr_leds = 23 - math.ceil(math.floor((23/100)*idle))
-    if curr_leds ~= prev_leds then
-	log.debug('actual idle:'..idle..', curr_leds:'..curr_leds..', prev_leds:'..prev_leds)
-	--[[
-	if curr_leds - prev_leds > 0 then
-	    -- turn on
-	    for i=prev_leds,curr_leds,1 do
-		serial.write('w', active_pins[i], ON)
-	    end
-	else
-	    -- turn off
-	    for i=prev_leds,curr_leds,-1 do
-		serial.write('w', active_pins[i], OFF)
-	    end
-	end
-	]]
-	serial.write('w', SERVO, curr_leds)
-	prev_leds=curr_leds
-	
-
+    curr_idle = 24 - math.ceil(math.floor((24/100)*idle))
+    if curr_idle ~= prev_idle then
+	log.debug('actual idle:'..idle..', curr_idle:'..curr_idle..', prev_idle:'..prev_idle)
+	serial.write('w', SERVO_PIN, curr_idle)
+	prev_idle=curr_idle
     end
     sleep(interval)
 end
 
 --- Print sub-module usage message
 function usage()
-    print("Usage: start.sh cpuidle [-h|[options]]")
-    print("Monitor the CPU idle percentage over a given time frame and display on boobie board.")
+    print("Usage: start.sh servoidle [-h|[options]]")
+    print("Monitor the CPU idle percentage over a given time frame and display using a servo gauge.")
+    print("Only works with Archonix Boobie Servo firmware!")
     print("-h|--help : print this message and exit")
     print("1st option : number of seconds between samples, default 1 sec")
     print("2nd option : number of samples to take, 2")
